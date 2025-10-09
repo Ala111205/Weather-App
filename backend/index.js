469a9481265e886865de7adbcecb2abf752ff0a0
Ala111205 <sadham070403@gmail.com>
2025-10-09 21:47:48 +0530

    front url

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: 'http://127.0.0.1:5500', 
+  origin: ['http://127.0.0.1:5500', 'https://weather-app-one-rosy-18.vercel.app/'],
   methods: ['GET', 'POST'],        
   credentials: true                
 }));
