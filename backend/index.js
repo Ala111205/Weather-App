b76104a009cf6f188bbb223583a9eaef2796ee9d
Ala111205 <sadham070403@gmail.com>
2025-10-25 12:43:38 +0530

    const

--- backend/index.js
+++ backend/index.js
@@ -53,7 +53,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-import rateLimit from "express-rate-limit";
+const rateLimit = require("express-rate-limit");
 
 // Trust Render’s proxy (you already have this)
 app.set("trust proxy", 1);
