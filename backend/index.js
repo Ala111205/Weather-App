fb6be5a634217b8670c61af34271c97236ff33fe
Ala111205 <sadham070403@gmail.com>
2025-10-25 12:46:07 +0530

    remove

--- backend/index.js
+++ backend/index.js
@@ -53,8 +53,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-const rateLimit = require("express-rate-limit");
-
 // Trust Render’s proxy (you already have this)
 app.set("trust proxy", 1);
 
